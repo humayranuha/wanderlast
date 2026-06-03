17a543e545b81a91540425547f82c128d09aa7c6
humayranuha <nuhahumayra24@gmail.com>
2026-06-03 11:54:00 +0600

    feat: no data animation added

---
+++ src/components/NoDataAnimation.js
@@ -0,0 +1,14 @@
+'use client';
+
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+export default function NotDataAnimation() {
+    return (
+        <DotLottieReact
+            src="/animations/not-data.lottie"
+            autoplay
+            loop
+            style={{ width: 350, height: 350 }}
+        />
+    );
+}
