3976ffe639432dca8710a62696c78eb4b3b47736
humayranuha <nuhahumayra24@gmail.com>
2026-06-04 01:18:26 +0600

    feat: add footer section

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
+import Footer from "@/components/shared/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
         <ClerkProvider>
           <Navbar />
           {children}
+          <Footer />
         </ClerkProvider>
       </body>
     </html>
