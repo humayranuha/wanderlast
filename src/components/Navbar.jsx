f2b93ddfbe2b9fd181491203e9b317db4b7fdac6
humayranuha <nuhahumayra24@gmail.com>
2026-06-03 11:19:29 +0600

    chore: install gravity ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import Link from 'next/link';
+import logo from '/public/assets/Wanderlast.png';
+import Image from 'next/image';
 
 const Navbar = () => {
     return (
         <div>
-            
+            <nav>
+                <div>
+                    <ul>
+                        <li><Link href='/'>Home</Link></li>
+                        <li><Link href='/'>Destinations</Link></li>
+                        <li><Link href='/'>My Bookings</Link></li>
+                        <li><Link href='/'>Admin</Link></li>
+                    </ul>
+                </div>
+                <div>
+                    <Image src={logo} alt='Wanderlast logo' unoptimized width={150} height={50} />
+                </div>
+                <div>
+                    
+                </div>
+            </nav>
         </div>
     );
 };
